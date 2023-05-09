414a0a145134fde077276927d3dc1cf4f71fbca2
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-09 22:19:04 +0900

    Update Class0509_02.java

--- j0509_/src/j0509_/Class0509_02.java
+++ j0509_/src/j0509_/Class0509_02.java
@@ -8,10 +8,11 @@ public class Class0509_02 {
 		int math = 89;
 		int total = kor+eng+math;
 		double avg = total/3.0;
+		
 		System.out.println("========================================");
-		System.out.println("국어  | \t영어  | \t수학  | \t합계  | \t평균");
+		System.out.println(" 국어 | \t영어  | \t수학  | \t합계  | \t평균");
 		System.out.println("========================================");
-		System.out.printf("%d  | \t%d   | \t%d   | \t%d  | \t%.2f\n",kor, eng, math, total, avg);
+		System.out.printf(" %d | \t%d   | \t%d   | \t%d  | \t%.2f\n",kor, eng, math, total, avg);
 	}
 
 }
