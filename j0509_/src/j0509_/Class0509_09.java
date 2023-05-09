cbc7b5409738009950af331cd7b27bb8c57f5e46
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-09 22:36:19 +0900

    Update Class0509_09.java

--- j0509_/src/j0509_/Class0509_09.java
+++ j0509_/src/j0509_/Class0509_09.java
@@ -11,14 +11,13 @@ public class Class0509_09 {
 		
 		System.out.println("문자열(숫자)을 입력하세요.");
 		Scanner scan = new Scanner(System.in);
-		char input = scan.next().charAt(3); // charAt(): 문자열에서 1개의 문자를 가져옴.
+		char input = scan.next().charAt(0); // charAt(): 문자열에서 1개의 문자를 가져옴.
 		
-		int result = input - '0'; // 1 		*cf. char -'0' = int
+		int result = input - '0'; // 1 		***cf. char -'0' = int
 		String content = (result%2==0)? "짝수" : "홀수";
 		
 		System.out.println("추출한 숫자: " +result);
 		System.out.println(content);
-		
 	}
 
 }
