8e17b35ecd14c7b573f1ffb9ffe549a18e5c3c7f
KitschKitsch <jss5794@naver.com>
2023-05-09 17:18:16 +0900

    0509 연산자

---
+++ j0509_/src/j0509_/Class0509_04.java
@@ -0,0 +1,40 @@
+package j0509_;
+
+import java.util.Scanner;
+
+public class Class0509_04 {
+
+	public static void main(String[] args) {
+		// 아이디, 패스워드, 이름을 입력받아 출력하시오.(각각 문자열, 정수형, 문자열)
+		// ---------------------------
+		// 아이디		패스워드		이름
+		// ---------------------------
+		// abcd		1111		홍길동
+
+		// 1. 변수 선언
+		String id = "";
+		String pw = ""; // 앞자리 0을 넣고 싶다면 문자열로!
+		String name = "";
+		
+		// 2. 스캐너 객체 만들기 (콘솔-System에서 입력-in)
+		Scanner scan = new Scanner(System.in);
+		
+		// 3. id, pw, name 입력 받기
+		System.out.println("아이디를 입력하세요");
+		id = scan.nextLine();
+		
+		System.out.println("패스워드를 입력하세요");
+		pw = scan.nextLine();
+				// scan.nextLine(); // nextInt()일 때의 enter키를 처리! 
+
+		System.out.println("이름을 입력하세요");
+		name = scan.nextLine();
+		
+		// 4. 출력
+		System.out.println("---------------------------");		
+		System.out.println("아이디 \t 패스워드 \t 이름");
+		System.out.println("---------------------------");
+		System.out.printf("%s \t %s \t %s \n", id, pw, name);
+	}
+
+}
---
+++ j0509_/src/j0509_/Class0509_11.java
@@ -0,0 +1,22 @@
+package j0509_;
+
+public class Class0509_11 {
+
+	public static void main(String[] args) {
+		/*
+		// 소수점 4째자리에서 반올림해서 출력하시오.
+		// 1.변수선언 2.출력
+		double pi = 3.141592;
+		double result = Math.round(pi*1000)/1000.0;
+//						 			  3142/1000.0
+		System.out.println(result); // 3.142
+		*/
+		
+		// 소수점 2째자리에서 반올림해서 출력하시오.
+		double pi = 3.7894;
+		double result = Math.round(pi*10)/10.0;
+		System.out.println(result); // 3.8
+		
+	}
+
+}
