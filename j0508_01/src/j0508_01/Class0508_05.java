b661fe9f4cd858bf19b1525e62e553b3a2693186
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-09 21:59:48 +0900

    Update Class0508_05.java

--- j0508_01/src/j0508_01/Class0508_05.java
+++ j0508_01/src/j0508_01/Class0508_05.java
@@ -4,17 +4,16 @@ public class Class0508_05 {
 
 	public static void main(String[] args) {
 		// 오버플로우
-		// 2147483647
+		// 21|4748|3647
 		int num = 2147483647;
 		System.out.println(num+1);
 		
 		int num1 = 1000000;
 		int num2 = 2000000;
-		System.out.println(num1*num2); // 2,000,000,000,000
+		System.out.println(num1*num2); // 2|0000|0000|0000
 		
 		int num3 = -2147483648;
 		System.out.println(num3-1);
-
 	}
 
 }
