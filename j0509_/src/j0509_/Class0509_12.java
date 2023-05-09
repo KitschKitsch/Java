3e790de08c384f85b39d1437eccdc2ec7c35f943
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-09 22:43:57 +0900

    Update Class0509_12.java

--- j0509_/src/j0509_/Class0509_12.java
+++ j0509_/src/j0509_/Class0509_12.java
@@ -8,8 +8,8 @@ public class Class0509_12 {
 		// 영문 소문자인지 비교
 		char ch = ' ';
 		
-		Scanner scan = new Scanner(System.in);
 		System.out.println("문자를 입력하세요.");
+		Scanner scan = new Scanner(System.in);
 		ch = scan.next().charAt(0);
 		
 		String result = (ch>='A' && ch<='Z')||(ch>='a' && ch<='z')? "영문자입니다." : "영문자가 아닙니다.";
