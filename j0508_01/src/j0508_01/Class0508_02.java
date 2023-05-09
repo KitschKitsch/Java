e0052e0bc2ed016b74a53bdc38ac6719ac554878
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-09 21:44:17 +0900

    Update Class0508_02.java

--- j0508_01/src/j0508_01/Class0508_02.java
+++ j0508_01/src/j0508_01/Class0508_02.java
@@ -1,15 +1,29 @@
-package j0508_01;
+package j0508_home;
 
-public class Class0508_02 {
-	
+public class Class0508_02_home {
+
 	public static void main(String[] args) {
+		
 		boolean power = true;
-		// int num = 1;
-		// int num2 = 1+ch; // 가능..?!
+		System.out.println(power);
+
 		char ch = 'A';
+		int num = 1+ch; // 가능! A = ASCII 코드 65
+		System.out.println(ch);
+		System.out.println(num);
+		
 		char ch2 = 65; // A = ASCII 코드 65
-		char ch3 = '\u0041'; // 16진수 
+		System.out.println(ch2);
+
+		char ch3 = '\u0041'; // 16진수 A 
+		System.out.println(ch3);
+
 		char tab = '\t';
+		System.out.println(tab);
+
+		System.out.println(""+tab+ch2);
+		System.out.println(""+ch+num); // 문자열로 같이 출력할꺼야!
+
 		byte b = 127;
 		short s = 32767;
 		int i = 100;
@@ -20,17 +34,7 @@ public class Class0508_02 {
 		float f = 3.14f;
 		double d = 3.14;
 		float f2 = 100f; // 자동 소수점
-		
-		// System.out.println(power);
-		// System.out.println(num2);
-		// System.out.println(ch);
-		System.out.println(ch2);
-		// System.out.println(ch3);
-		// System.out.println(tab);
-		System.out.println(""+tab+ch2);
-		// System.out.println(""+ch+num); // 문자열로 같이 출력할꺼야!
 		System.out.println(f2);
-		
-	}// main
+	}
 
-}// class
+}
