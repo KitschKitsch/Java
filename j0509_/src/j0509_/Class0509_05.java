83e9c33416eaeaf7bada4088b46eb3d7eca3012d
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-09 22:27:00 +0900

    Update Class0509_05.java

--- j0509_/src/j0509_/Class0509_05.java
+++ j0509_/src/j0509_/Class0509_05.java
@@ -6,14 +6,15 @@ public class Class0509_05 {
 		int num1 = 3;
 		int num2 = 4;
 		int num3 = 5;
-		// int result = (num2*num3)+num1;
-		// System.out.println(result);
 		
-		int result = ++num3; // 5 + 1 = 6 *먼저 num3값을 1증가시키고 result에 대입시켜줘~
+		int result = (num2*num3)+num1;
 		System.out.println(result);
 		
-		int result2 = num2++; // *먼저 num2값을 result2에 대입하고 1증가시켜줘~
+		int result2 = ++num3; // 5 + 1 = 6 *먼저 num3값을 1증가시키고 result2에 대입시켜줘~
 		System.out.println(result2);
+		
+		int result3 = num2++; // *먼저 num2값을 result3에 대입하고 1증가시켜줘~
+		System.out.println(result3);
 		System.out.println("num2: "+num2);
 	}
 
