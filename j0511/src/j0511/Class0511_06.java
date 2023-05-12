108eb1161b8902dd1d619ac110b0dc8e61de430e
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-12 09:33:44 +0900

    Update Class0511_06.java

--- j0511/src/j0511/Class0511_06.java
+++ j0511/src/j0511/Class0511_06.java
@@ -16,7 +16,7 @@ public class Class0511_06 {
 		//score[0] = 1; score[1] = 2; score[2] = 3; score[3] = 4;	score[4] = 5;
 		//score[5] = 6; score[6] = 7; score[7] = 8; score[8] = 9;	score[9] = 10;
 		int[] input = {1,2,3,4,5,6,7,8,9,10}; // 10개 배열 자동 생성
-		System.out.println("" +score[0] +score[1] +score[2]);
+		System.out.println("" +input[0] +input[1] +input[2]);
 	}
 
 }
