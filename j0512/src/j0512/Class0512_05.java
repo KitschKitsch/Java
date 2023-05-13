35a98d4f350345ba78b0c53c014d8cdddf5b9cd4
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-14 00:23:47 +0900

    Update Class0512_05.java

--- j0512/src/j0512/Class0512_05.java
+++ j0512/src/j0512/Class0512_05.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 public class Class0512_05 {
 
 	public static void main(String[] args) {
-		// 10진수를 2진수로 변경하는 프로그램
-		Scanner scan = new Scanner(System.in);
-		// int[] data = new [10];
-		// 			      [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]
-		String[] data = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001"};
+		// 10진수를 2진수로 변환해주는 프로그램
 		
+		Scanner scan = new Scanner(System.in);
+		
+		// 1)배열선언
+		String[] binary = {"0000","0001","0010","0011","0100",
+					  	   "0101","0110","0111","1000","1001"};
 		int[] num = new int[5];
-		// 1. 숫자입력
-		for (int i=0; i<5; i++) {
-			System.out.println("10진수를 입력하세요.");
+		
+		// 2)숫자입력
+		for (int i=0; i<num.length; i++) {
+			System.out.println("숫자를 입력하세요.>> ");
 			num[i] = scan.nextInt();
 		}
 		
-		// 2. 2진코드 출력
-		for (int i=0; i<5; i++) {
-			System.out.print(data[num[i]] +" "); //출력
+		// 3)이진코드출력
+		System.out.print("이진코드: ");
+		for (int i=0; i<num.length; i++) {
+			System.out.print(binary[num[i]] +" ");
 		}
-		System.out.println();
 	}
 
 }
