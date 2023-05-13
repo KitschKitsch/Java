6aa72192f7db9bf3fbe4aa43afca7a8296754a12
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-14 00:24:10 +0900

    Update Class0512_06.java

--- j0512/src/j0512/Class0512_06.java
+++ j0512/src/j0512/Class0512_06.java
@@ -5,36 +5,39 @@ import java.util.Scanner;
 public class Class0512_06 {
 
 	public static void main(String[] args) {
+		// 1차원배열 vs 2차원배열
+		
 		Scanner scan = new Scanner(System.in);
 		
-		// 1차원 배열
-//		int[] num = new int[3];
-//		for (int i=0; i<num.length; i++) {
-//			System.out.println("점수를 입력하세요.>> ");
-//			num[i] = scan.nextInt();
-//		}
-//		
-//		for (int i=0; i<num.length; i++) {
-//			System.out.println(num[i]);
-//		}
+		// 1차원배열 - 배열선언,입력,출력
+		int[] num = new int[3];
 		
-		// 2차원 배열
-		int[][] score = new int[3][4]; //3행 4열
-		// 입력
-		for (int i=0; i<score.length; i++) { //score.length = 3행
-			for (int j=0; j<score[i].length; j++) { //score[i].length = 4열
-				System.out.println("2차원 점수를 입력하세요.>>");
-				score[i][j] = scan.nextInt();
-			}
+		for (int i=0; i<num.length; i++) {
+			System.out.println("점수를 입력하세요.>> ");
+			num[i] = scan.nextInt();
 		}
+
+		for (int i=0; i<num.length; i++) {
+			System.out.print(num[i] +" ");
+		}
+		System.out.println();
 		
-		//출력
-		for (int i=0; i<score.length; i++) { //score.length = 3행
-			for (int j=0; j<score[i].length; j++) { //score[i].length = 4열
-				System.out.print(score[i][j] +"\t");
+		// 2차원배열 - 배열선언,입력,출력
+		int[][] num2 = new int[3][4];//3행*4열
+		
+		for (int i=0; i<num2.length; i++) {//3행
+			for (int j=0; j<num2[i].length; j++) {//4열
+				System.out.println("2차원 점수를 입력하세요.>> ");
+				num2[i][j] = scan.nextInt();
+			}
+		}//for문(입력)
+		
+		for (int i=0; i<num2.length; i++) {//3행
+			for (int j=0; j<num2[i].length; j++) {//4열
+				System.out.print(num2[i][j] +"\t");
 			}
 			System.out.println();
-		}
+		}//for문(출력)
 	}
 
 }
