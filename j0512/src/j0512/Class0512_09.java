e8edeeaaacb1a63ab1476320508aaab1f893d787
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-14 00:24:55 +0900

    Update Class0512_09.java

--- j0512/src/j0512/Class0512_09.java
+++ j0512/src/j0512/Class0512_09.java
@@ -5,43 +5,42 @@ import java.util.Scanner;
 public class Class0512_09 {
 
 	public static void main(String[] args) {
-		// 이름(1차원)을 넣고 국어,영어,수학,합계(2차원), 평균(1차원) 출력
+		// 학생 5명의 이름과 국어,영어,수학 성적을 입력하고 합계와 평균까지 출력하는 프로그램
+		
 		Scanner scan = new Scanner(System.in);
 		
-		String[] name = new String[5];
-		int[][] score = new int[5][4];
-		double[] avg = new double[5];
-		String[] title = {"이름","국어","영어","수학","합계","평균"};
+		// 1)배열선언
+		String[] name = new String[5];//이름배열
+		int[][] score = new int[5][4];//국영수합계 배열
+		double[] avg = new double[5];//평균배열
+		String[] title = {"이름","국어","영어","수학","합계","평균"};//표상단배열
 		
-		//입력
+		// 2)성적입력
 		for (int i=0; i<score.length; i++) {
-			System.out.println(i+1 +"번째 학생: ");
-			System.out.println("이름 입력>> ");
+			System.out.println((i+1) +"번째 학생: ");
+			System.out.println("이름을 입력하세요.>> ");
 			name[i] = scan.next();
 			
 			for (int j=0; j<score[i].length-1; j++) {
-				System.out.println(title[j+1] +" 점수 입력>> ");
+				System.out.println(title[j+1] +"성적을 입력하세요.>> ");
 				score[i][j] = scan.nextInt();
 			}
-			//합계
-			score[i][3] = score[i][0]+score[i][1]+score[i][2];
-			//평균
-			avg[i] = score[i][3]/3.0;
+			score[i][3] = score[i][0]+score[i][1]+score[i][2];//4열에 합계
+			avg[i] = score[i][3]/3.0;//평균배열에 따로 저장
 		}
-
-		//출력(title)
+		
+		// 3)성적출력
 		for (int i=0; i<title.length; i++) {
 			System.out.printf("%s\t", title[i]);
 		}
 		System.out.println();
-		System.out.println("-----------------------------------------");
-		//출력(name+score)
+		System.out.println("--------------------------------------------------");
 		for (int i=0; i<score.length; i++) {
 			System.out.print(name[i] +"\t");
 			for (int j=0; j<score[i].length; j++) {
 				System.out.print(score[i][j] +"\t");
 			}
-			System.out.printf("%.2f", avg[i]); //평균출력
+			System.out.printf("%.2f", avg[i]);
 			System.out.println();
 		}
 	}
