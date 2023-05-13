e8a89a2a8187aec50a4b0d2330ce5913c7fd2beb
KitschKitsch <132968547+KitschKitsch@users.noreply.github.com>
2023-05-14 00:23:31 +0900

    Update Class0512_04.java

--- j0512/src/j0512/Class0512_04.java
+++ j0512/src/j0512/Class0512_04.java
@@ -3,37 +3,35 @@ package j0512;
 import java.util.Scanner;
 
 public class Class0512_04 {
-
+	
 	public static void main(String[] args) {
-		// 10진수를 2진수로 변경하는 프로그램
+		// 숫자를 입력받아 십진코드와 이진코드를 출력하는 프로그램
+		
 		Scanner scan = new Scanner(System.in);
 		
-		String[] binary = {"0000", "0001", "0010", "0011",
-						   "0100", "0101", "0110", "0111",
-						   "1000", "1001"};
+		// 1)배열선언 - 2진코드배열, 입력배열
+		String[] binary = {"0000", "0001", "0010", "0011", "0100",
+						   "0101", "0110", "0111", "1000", "1001"};//0~9의 이진코드 배열
+		int[] num = new int[4];//10진수 입력받을 배열. 일단 4자리만!
 		
-		int[] num = new int[4];
-		
-		// 1개씩 받기
-		// 1025 - 0001 0000 0010 0101
+		// 2)숫자입력
 		for (int i=0; i<num.length; i++) {
 			System.out.println("번호를 입력하세요.>> ");
 			num[i] = scan.nextInt();
 		}
 		
-		// 10진코드 출력
-		System.out.print("10진코드: ");
+		// 3)십진코드 출력
+		System.out.print("십진코드: ");
 		for (int i=0; i<num.length; i++) {
 			System.out.print(num[i] +" ");
 		}
 		System.out.println();
 		
-		// 2진코드로 번호출력
-		System.out.print("2진코드: ");
+		// 4)이진코드 출력
+		System.out.print("이진코드: ");
 		for (int i=0; i<num.length; i++) {
 			System.out.print(binary[num[i]] +" ");
 		}
-		System.out.println();
 	}
 
 }
